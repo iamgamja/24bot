481c5ee260fae64e99e6a5ffe12cead58c578d82
iamgamja <68046824+iamgamja@users.noreply.github.com>
2020-10-20 15:22:42 +0900

    Update myBot.py

--- myBot.py
+++ myBot.py
@@ -70,9 +70,11 @@ def 시간():
 async def on_ready():
 	# print('시작')
 	await client.change_presence(status=discord.Status.online, activity=discord.Activity(name=",도움", type=discord.ActivityType.listening))
+	startmsg = await client.get_channel(762916201654386701).send(f"{시간()}, 시작")
+	
 	while True:
-		await client.get_channel(762916201654386701).send(f"{시간()}, 시작")
-		time.sleep(1)
+		time.sleep(100)
+		await startmsg.edit(f"{시간()}, 작동중")
 
 @client.event
 async def on_message(message):
