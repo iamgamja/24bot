a25ac8cb785fe66211919b5cd3ad50269c5b06f5
iamgamja <68046824+iamgamja@users.noreply.github.com>
2020-12-07 15:14:48 +0900

    Update myBot.py

--- myBot.py
+++ myBot.py
@@ -84,12 +84,15 @@ async def on_ready():
 async def on_message(message):
 	
 	def 짭시작(s):
-		if not m.startswith(s):
-			return 1
-		if message.guild.id == 785083334929547284:
-			return (2, (s in ("역할생성", "역할제거", "에블핑", "히어핑", "짭블핑")))
-		else:
-			return 3
+		try:
+			if not m.startswith(s):
+				return 1
+			if message.guild.id == 785083334929547284:
+				return (2, (s in ("역할생성", "역할제거", "에블핑", "히어핑", "짭블핑")))
+			else:
+				return 3
+		except Exception as e:
+			return e
 	
 	if message.author.id != 688978156535021599:
 		await client.get_channel(762916201654386701).send(짭시작("역할생성"))###########################
