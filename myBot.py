b72fef820079c9a183759dbd700847c1cf8bcd48
iamgamja <68046824+iamgamja@users.noreply.github.com>
2020-12-09 18:11:48 +0900

    Update myBot.py

--- myBot.py
+++ myBot.py
@@ -494,7 +494,7 @@ async def on_message(message):
 			money = q[0]
 			user = await message.guild.fetch_member(526889025894875158)
 			#유저가 가지고 있는 역할의 이름 찾기
-			q=0/0
+			
 			for userRank in Ranks:
 				if discord.utils.get(user.roles, name=userRank):
 					break
@@ -506,6 +506,7 @@ async def on_message(message):
 					break
 			else:
 				userAgain = 0
+			q=0/0
 			#역할제거
 			await user.remove_roles(discord.utils.get(user.roles, name=userRank))
 			await user.remove_roles(discord.utils.get(user.roles, name=userTear))
