489c69a46bc9e716e316735a3e9d3484a193c5d4
iamgamja <68046824+iamgamja@users.noreply.github.com>
2020-12-19 18:22:50 +0900

    Update myBot.py

--- myBot.py
+++ myBot.py
@@ -708,7 +708,7 @@ async def on_message(message):
 						await client.get_channel(783513080352079872).send(f"{message.author}님이 실드를 구매함")
 				else:
 					money = round(-dmoney+dmoney*dp)
-				await client.get_channel(762916201654386701).send((userRank, userTear, userAgain), (userTotalRank, money))
+				await client.get_channel(762916201654386701).send(((userRank, userTear, userAgain), (userTotalRank, money)))
 				#랭크적용...
 				userTotalRank += money
 				await client.get_channel(762916201654386701).send((userTotalRank, money))
