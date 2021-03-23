baeb6871ea474d8f8c601626d1c58bc07d47661d
iamgamja <68046824+iamgamja@users.noreply.github.com>
2021-03-23 11:58:54 +0900

    Update myBot.py
    전부 다 처리하고 체크 반응 달도록 변경함

--- myBot.py
+++ myBot.py
@@ -1061,7 +1061,7 @@ async def on_message(message):
 			
 			if case == 2:
 				await message.channel.send("랭크업에 성공하였습니다!")
-			await message.add_reaction(체크)
+		await message.add_reaction(체크)
 ##########
 
 
