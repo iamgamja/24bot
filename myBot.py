b5e018c6404cd884ab752efef571a76f889cc097
iamgamja <68046824+iamgamja@users.noreply.github.com>
2021-05-05 14:18:40 +0900

    Update myBot.py

--- myBot.py
+++ myBot.py
@@ -449,7 +449,7 @@ async def on_message(message):
                 order = "date"
             ).execute()['items'][0]
 
-            return video['snippet']['title'] + '\n' + get_sumbnail_by_url(video['id']['videoId'])
+            return video['snippet']['title'] + '\n' + get_thumbnail_by_url(video['id']['videoId'])
 
         if message.author.id == 405664776954576896 and message.channel.id in (766932314973929527, 783516524685688842, 784228694940057640, 794146499034480661):
             #랭크업, 시간, 도박장, 도박2 에서의 슷칼봇 메시지 삭제
