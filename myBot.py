82b1381cb89ac10ee15f69ffea7252d27ebe901f
iamgamja <hmmthinking@naver.com>
2021-05-27 16:46:34 +0900

    Update myBot.py

--- myBot.py
+++ myBot.py
@@ -841,6 +841,7 @@ async def on_message(message):
             await message.channel.send(outputmsg[:2000-3]+'...' if len(outputmsg) > 2000 else outputmsg)
             
         elif 시작(",await코드") and 관리():
+            m = ' '.join(m.split(' ')[1:])
             
             exec('global awaitFunction\nasync def awaitFunction():\n' + '\n'.join(list(map(lambda x: '    '+x, m.split('\n')[:-1]))) + '\n    return ' + m.split('\n')[-1])
             outputmsg = await awaitFunction()
