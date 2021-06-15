3d2f5d90171bed0861dc0832db45712209936df0
iamgamja <hmmthinking@naver.com>
2021-06-15 09:28:18 +0900

    Create imports.py

---
+++ imports.py
@@ -0,0 +1,19 @@
+import discord
+import time, datetime, random, os, math, string, sys
+
+import asyncio
+import traceback
+import requests
+
+from bs4 import BeautifulSoup
+
+import json
+import html
+
+from googleapiclient.discovery import build
+
+import re
+
+import google.oauth2.credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
