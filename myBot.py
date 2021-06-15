9ea58c8df90f88a1894249c3e624ff9e8dfaceee
iamgamja <hmmthinking@naver.com>
2021-06-15 09:29:12 +0900

    Update myBot.py

--- myBot.py
+++ myBot.py
@@ -1,22 +1,4 @@
-import discord
-import time, datetime, random, os, math, string, sys
-
-import asyncio
-import traceback
-import requests
-
-from bs4 import BeautifulSoup
-
-import json
-import html
-
-from googleapiclient.discovery import build
-
-import re
-
-import google.oauth2.credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from imports import *
 
 
 DEVELOPER_KEY = os.environ["DEVELOPER_KEY"]
