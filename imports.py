35c5c452e79cbe3c4c24e7587269cb492658603e
iamgamja <hmmthinking@naver.com>
2021-06-15 09:33:07 +0900

    Update imports.py

--- imports.py
+++ imports.py
@@ -17,3 +17,6 @@ import re
 import google.oauth2.credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+
+
+this_is_my_DEVELOPER_KEY = os.environ["DEVELOPER_KEY"]
