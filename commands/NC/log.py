e621b81451088405b5f30db990182549023149a6
iamgamja <hmmthinking@naver.com>
2021-07-03 13:23:43 +0900

    Delete commands/NC directory

--- commands/NC/log.py
+++
@@ -1,9 +0,0 @@
-async def log(self, s):
-    s = str(s)
-    
-    print(s)
-    if len(s) > 2000:
-        await self.get_channel(762916201654386701).send(s[:2000])
-        await log(s[2000:])
-    else:
-        await self.get_channel(762916201654386701).send(s)
