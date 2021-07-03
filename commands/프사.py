0f4ae10acada621cb352ac9ceae50ec890424f0f
iamgamja <hmmthinking@naver.com>
2021-07-03 13:08:20 +0900

    Update and rename vmtk.py to 프사.py

--- commands/프사.py
+++ commands/프사.py
@@ -1,2 +1,2 @@
-async def vmtk(discord, client, message):
+async def 프사(discord, client, message):
     await message.channel.send(embed=discord.Embed(title="프사", color=0xffccff).set_image(url=message.author.avatar_url))
